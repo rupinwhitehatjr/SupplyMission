bdec739f497072dc20d42513c6f5d4c172d2524d
shaukath300487 <70494399+shaukath300487@users.noreply.github.com>
2020-10-17 13:26:44 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -55,7 +55,7 @@ function draw() {
 }
 
 function keyPressed() {
- if (keyCode === DOWN_ARROW) {
+ if (keyCode === 25) {
     // Look at the hints in the document and understand how to make the package body fall only on
     
   }
