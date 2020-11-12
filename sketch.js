456367a04867dc2e4ce2987f694d71b5f341add4
rupinwhitehatjr <61224617+rupinwhitehatjr@users.noreply.github.com>
2020-11-12 17:54:06 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -56,7 +56,8 @@ function draw() {
 
 function keyPressed() {
  if (keyCode === DOWN_ARROW) {
-    // Look at the hints in the document and understand how to make the package body fall only on
+    // Look at the hints in the document and understand how to make the package body fall only on press of the Down arrow key.
+
     
   }
 }
